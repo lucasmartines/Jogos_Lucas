a92f935a67486a2631b5218209c85c536e938d82
lucasmartiness <lucasalcantarilla97@outlook.com>
2018-11-05 16:15:02 -0200

    loading correção 1

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,9 +1,10 @@
 
 
-// Este evendo é acionado após o carregamento da página
-$(document).ready(function() {
-    //Após a leitura da pagina o evento fadeOut do loader é acionado, esta com delay para ser perceptivo em ambiente fora do servidor.
-    $(".loader").hide();
-});
+        // Este evendo é acionado após o carregamento da página
+        $(document).ready(function() {
+            //Após a leitura da pagina o evento fadeOut do loader é acionado, esta com delay para ser perceptivo em ambiente fora do servidor.
+            $(".loader").hide();
+        });
 
         
+    </script>
