bfc3e6a9248641a09fd83bfb4a843e2bbc0ddb14
lucasmartiness <lucasalcantarilla97@outlook.com>
2018-11-05 15:18:04 -0200

    adicionado loading

---
+++ public/js/loader.js
@@ -0,0 +1,9 @@
+
+
+// Este evendo é acionado após o carregamento da página
+$(document).ready(function() {
+    //Após a leitura da pagina o evento fadeOut do loader é acionado, esta com delay para ser perceptivo em ambiente fora do servidor.
+    $(".loader").hide();
+});
+
+        
