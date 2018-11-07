f0cde2cc86cade671bb553a0d49be09a9388c146
lucasmartiness <lucasalcantarilla97@outlook.com>
2018-11-06 22:40:33 -0200

    otimização

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ app.use( express.static('public') )
 
 const port = process.env.PORT || 3000;
 
+app.get('*', function(req, res){
+  res.sendFile( __dirname + '/public/404.html' )
+});
 
 app.listen( port , ()=>{
 	console.log("servidor rodando")
