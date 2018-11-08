272a0ebd6f01f750bfda5dbeb71e336e42a3cb3c
lucasmartiness <lucasalcantarilla97@outlook.com>
2018-11-08 02:51:19 -0200

    rack para evitar que o heroku durma

--- server.js
+++ server.js
@@ -13,3 +13,9 @@ app.listen( port , ()=>{
 	console.log("servidor rodando")
 
 } )
+
+var http = require("http");
+setInterval(function() 
+{
+    http.get("https://redmoonstudio.herokuapp.com/index.html");
+}, 600000); // every 5 minutes (300000)
